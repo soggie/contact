f4eaa2a8b448aa1f25d8a6bf30a995466138a6b0
Ruben LZ Tan <soggie@gmail.com>
2018-04-17 17:08:18 +0800

    Final version, working properly

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ const sendmail = (name, email, company, phone, inquiry) => {
   const user = process.env.SENDMAIL_SMTP_USER
   const pass = process.env.SENDMAIL_SMTP_PASS
   const secure = process.env.SENDMAIL_SMTP_SECURE || true
-  const port = process.env.SENDMAIL_SMTP_PORT || 587
+  const port = process.env.SENDMAIL_SMTP_PORT || 465
   const subject = `[Website] New inquiry from ${email}`
 
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
+    host: host,
+    port: port,
     secure: true,
     auth: {
       user: user,
@@ -23,14 +23,18 @@ const sendmail = (name, email, company, phone, inquiry) => {
   })
 
   const mail = {
-    from: '',
+    from: user,
     to: to,
     subject: subject,
-    text: `Test inquiry`,
-    html: '<p>Test</p>'
+    html: `
+      <p>We have received an inquiry from the website. Details are as follows:</p>
+      <p>Full name: ${name}</p>
+      <p>Company: ${company}</p>
+      <p>Email: ${email}</p>
+      <p>Phone: ${phone}</p>
+      <p>Inquiring: ${inquiry}</p>
+    `
   }
-
-  console.log(user, pass, mail)
 
   return new Promise((resolve, reject) => {
     transporter.sendMail(mail, (err, info) => {
@@ -44,13 +48,26 @@ const sendmail = (name, email, company, phone, inquiry) => {
 }
 
 const server = http.createServer((req, res) => {
+  // Used for testing only
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Request-Method', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST')
+  res.setHeader('Access-Control-Allow-Headers', 'authorization, content-type')
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200)
+    res.end()
+    return
+  }
+
   jsonBody(req, {}, (err, body) => {
+    if (!body) return
+
     // Take the body and check for the proper fields
-    const name = body.name + ''
+    const name = body.fullname + ''
     const email = body.email + ''
     const company = body.company + ''
     const phone = body.phone + ''
-    const inquiry = body.inquiry + ''
+    const inquiry = body.type + ''
 
     // Make sure that all fields are filled up
     const errors = {}
@@ -66,6 +83,11 @@ const server = http.createServer((req, res) => {
     }
 
     // TODO: Add error checks
+    if (errors.length > 0) {
+      res.writeHead(400)
+      res.end(JSON.stringify(errors))
+      return
+    }
 
     sendmail(name, email, company, phone, inquiry)
       .then(info => res.end('Done'))
